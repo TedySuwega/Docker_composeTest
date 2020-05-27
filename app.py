15ba39f3f186b24429d1f151275698c26e6c5b76
tedy_suwega <tedy.suwega@alodokter.com>
2020-05-27 08:02:19 +0700

    add greetings route

--- app.py
+++ app.py
@@ -23,9 +23,13 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
-    # count = get_hit_count()
-    # return 'Hello my new friend!. \n We meet {} times today.'.format(count)
-    return 'Heloo my new friend from the other universe!'
+    count = get_hit_count()
+    return 'Hello my new friend!. \n We meet {} times today.'.format(count)
+    
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/greetings')
+def greetings():
+     return 'Heloo my new friend from the other universe!'
+
+# if __name__ == "__main__":
+#     app.run()
